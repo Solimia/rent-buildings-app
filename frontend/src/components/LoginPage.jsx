2b36f38b1045298a88e378d3c8fafc91f49e5cd3
Solimia <solomiagavlisina@gmail.com>
2025-11-09 22:50:14 +0200

    eee

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./LoginPage.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram, faFacebook, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 
 export default function LoginPage() {
     const [isActive, setIsActive] = useState(false);
@@ -15,11 +14,6 @@ export default function LoginPage() {
             <div className="form-container sign-up">
                 <form>
                     <h1 className="h1">Create Account</h1>
-                    <div class="social-icons">
-                        <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
-                    </div>
 
                     <span></span>
                     <input type="text" placeholder="Name" />
