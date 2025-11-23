6caae8ec5ce56710a0ae259d5d25db6a12476eaa
Maks <rubelmaksum2404@gmail.com>
2025-11-23 22:43:58 +0200

    connectbackend

--- frontend/src/components/Animationblox.jsx
+++ frontend/src/components/Animationblox.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "./Animationblox.css"
 import MotionDiv from './MotionDiv'
 import FilterMenu from './FilterMenu.jsx'
@@ -6,18 +6,54 @@ import FilterBlok from './FilterBlok.jsx'
 import FilterMenu2 from './FilterMenu2.jsx'
 import SearchComponent from './SearchComponent.jsx'
 import { CounterContext } from '../context/counter_context.jsx'
+import axios from 'axios'
 export default function Animationblox() {
     const { contheme } = useContext(CounterContext);
+    const [data, setData] = useState(null);
+
 
     useEffect(() => {
         document.documentElement.dataset.theme = contheme;
     }, [contheme]);
 
+    async function fetchData() {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/Houses`);
+        const data = response.data;
+        console.log("Fetched data:", data);
+        setData(data);
+    }
+
+    // fetchData();
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+
     return (
         <>
-            <div className="RowWrapper">
-
+            <div className='SideBarWrapItemParent'>
+                <FilterBlok></FilterBlok>
                 <div className="SideBarWrapItem">
+
+                    <SearchComponent></SearchComponent>
+                    <FilterMenu></FilterMenu>
+                    <FilterMenu2 Title={"Bedrooms"}></FilterMenu2>
+                    <FilterMenu2 Title={"Bathrooms"}></FilterMenu2>
+                    <FilterMenu2 Title={"Bathrooms"}></FilterMenu2>
+                    <FilterMenu2 Title={"Bathrooms"}></FilterMenu2>
+                    <FilterMenu2 Title={"Bathrooms"}></FilterMenu2>
+
+                </div>
+            </div>
+
+
+            <div className="RowWrapper">
+                {/* 
+                <div className="SideBarWrapItem">
+                    
+                </div> */}
+
+                <div className="SideBarWrapItemOpacity">
                     <FilterBlok></FilterBlok>
                     <SearchComponent></SearchComponent>
                     <FilterMenu></FilterMenu>
@@ -25,40 +61,16 @@ export default function Animationblox() {
                     <FilterMenu2 Title={"Bathrooms"}></FilterMenu2>
                 </div>
 
-
-
-
-                <div className='Animationblox-Container'>
-                    <div className='SecondCol'>
-                        <MotionDiv></MotionDiv>
-                        <MotionDiv></MotionDiv>
-                        <MotionDiv></MotionDiv>
-
-                    </div>
-                    <div className='SecondCol'>
-                        <MotionDiv></MotionDiv>
-                        <MotionDiv></MotionDiv>
-                        <MotionDiv></MotionDiv>
-
-
-                    </div>
-                    <div className='SecondCol'>
-                        <MotionDiv></MotionDiv>
-                        <MotionDiv></MotionDiv>
-                        <MotionDiv></MotionDiv>
-
-
-                    </div>
-                    <div className='SecondCol'>
-                        <MotionDiv></MotionDiv>
-                        <MotionDiv></MotionDiv>
-                        <MotionDiv></MotionDiv>
-
-
-                    </div>
+                <div className='Animationblox-Container-Flex'>
+                    {
+                        data && data.map((item) => (
+                            <MotionDiv key={item.id} mainimgUrl={item.mainimgUrl} pricePerNight={item.pricePerNight}
+                                address={item.address} ratingf={item.rating} title={item.title}></MotionDiv>
+                        ))
+                    }
                 </div>
 
-            </div>
+            </div >
 
 
 
